253901e866c89f2afa82e5431d1e4485810aed0e
Монге-Байир <113677897+Monge-Bayir@users.noreply.github.com>
2025-06-06 01:27:00 +0300

    создана логика для логина с токеном

--- app/users/auth.py
+++ app/users/auth.py
@@ -1,4 +1,9 @@
+import datetime
+import jwt
+from asyncpg.pgproto.pgproto import timedelta
 from passlib.context import CryptContext
+from pydantic import EmailStr
+from app.users.dao import UserDao
 
 pwd_content = CryptContext(schemes=['bcrypt'], deprecated='auto')
 
@@ -8,3 +13,20 @@ def get_password_hash(password: str) -> str:
 
 def verify_password(plain_password, hashed_password):
     return pwd_content.verify(plain_password, hashed_password)
+
+
+def create_access_token(data: dict) -> str:
+    to_encode = data.copy()
+    expire = datetime.datetime.utcnow() + timedelta(minutes=30)
+    to_encode.update({'exp': expire})
+    encoded_jwt = jwt.encode(
+        to_encode, 'asdlajsdasASDASD', 'HS256'
+    )
+    return encoded_jwt
+
+
+async def authenticate_user(email: EmailStr, password: str):
+    user = await UserDao.find_one_or_none(email=email)
+    if not user and verify_password(password, user.password):
+        return None
+    return user
--- app/users/routes.py
+++ app/users/routes.py
@@ -1,7 +1,7 @@
-from fastapi import APIRouter, HTTPException
-from app.users.schemas import SUserRegister
+from fastapi import APIRouter, HTTPException, Response, status
+from app.users.schemas import SUserAuth
 from app.users.dao import UserDao
-from app.users.auth import get_password_hash
+from app.users.auth import get_password_hash, authenticate_user, create_access_token
 
 router = APIRouter(
         prefix='/auth',
@@ -10,9 +10,19 @@ router = APIRouter(
 
 
 @router.post('/register')
-async def register_user(user_data: SUserRegister):
+async def register_user(user_data: SUserAuth):
     existing_user = await UserDao.find_one_or_none(email=user_data.email)
     if existing_user:
         raise HTTPException(status_code=500)
     hashed_password = get_password_hash(user_data.password)
     await UserDao.add(email=user_data.email, hashed_password=hashed_password)
+
+
+@router.post('/login')
+async def login_user(response: Response, user_data: SUserAuth):
+    user = await authenticate_user(email=user_data.email, password=user_data.password)
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+    access_token = create_access_token({'sub': user.id})
+    response.set_cookie('booking_access_token', access_token, httponly=True)
+    return access_token
